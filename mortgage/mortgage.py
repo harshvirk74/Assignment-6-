fd828e680a08aee5550194b80fcafe6cbc72df6d
Harshdeep Singh Virk <hvirk2@academic.rrc.ca>
2023-11-08 16:32:27 -0600

    Added date and my name to the all the required files

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -1,7 +1,7 @@
 """
 Description: A class meant to manage Mortgage options.
-Author: {Student Name}
-Date: {Date}
+Author: Harshdeep Singh Virk 
+Date: 08/11/2023
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
