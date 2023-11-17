13c033d255f0c0332314543dc6974741b245b7a5
Harshdeep Singh Virk <hvirk2@academic.rrc.ca>
2023-11-16 23:45:00 -0600

    Define __repr__ test cases

--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -2,7 +2,7 @@
 Description: A class used to test the Mortgage class.
 Author: Harshdeep Singh Virk 
 Date: 8/November/2023
-Usage: Use the tests encapsulated within this class to test the MortgagePayment class.
+Usage: Use the tests encapsulated within this class to test the Mortgage Payment class.
 """
 
 from unittest import TestCase
