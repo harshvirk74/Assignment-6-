6ac2254f4ebfa0659dfce3819e207129c08b2201
Harshdeep Singh Virk <hvirk2@academic.rrc.ca>
2023-11-16 23:36:29 -0600

    Define Accessors and Mutators for Frequency

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -1,7 +1,7 @@
 """
 Description: A class meant to manage Mortgage options.
 Author: Harshdeep Singh Virk 
-Date: 08/11/2023
+Date: 08/NOV/2023
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
