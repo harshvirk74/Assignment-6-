18e65cf397d46c557e4c98c5dd0480574edd5e04
Harshdeep Singh Virk <hvirk2@academic.rrc.ca>
2023-11-16 23:26:46 -0600

    Defined Accessors and Mutators for Loan Amount

--- main.py
+++ main.py
@@ -2,48 +2,52 @@
 Description: A client program written to verify accuracy of and 
 calculate payments for PiXELL River Mortgages.
 Author: ACE Faculty
-Edited by: {Student Name}
-Date: {Date}
+Edited/ by: Harshdeep Singh Virk 
+Date: 08/11/2023
 """
+# Import necessary modules from the mortgage package
+from mortgage import Mortgage
+from pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
 
-### REQUIREMENT
-### ADD IMPORT STATEMENTS FOR THE MORTGAGE CLASS, THE 
-### MORTGAGERATE AND MORTGAGEFREQUENCY ENUMERATIONS AND THE 
-### VALID_AMORTIZATION LIST
-
-
-
-### REQUIREMENT
-### ENCLOSE THE FOLLOWING 'WITH OPEN' BLOCK IN A 'TRY-EXCEPT' BLOCK WHICH 
-### WILL CATCH A 'FILENOTFOUNDERROR' EXCEPTION
-with open ("data\\pixell_river_mortgages.txt","r") as input:
-    print("**************************************************")
-    
-    for data in input:
-        items = data.split(",")
-        
-        try:
-            amount = float(items[0])
-            rate = items[1]
-            amortization = items[2]
-            frequency = items[3]
-
-            ### REQUIREMENT:
-            ### INSTANTIATE A MORTGAGE OBJECT USING THE VALUES
-            ### FOR AMOUNT, RATE, FREQUENCY AND AMORTIZATION ABOVE.
-
-            
-            ### REQUIREMENT:
-            ### PRINT THE MORTGAGE OBJECT
-
-        except ValueError as e:
-            # This except block will catch Explicit exceptions: 
-            # Those raised by the programmer in the Mortgage class.
-            print(f"Data: {data.strip()} caused Exception: {e}")
-        
-        except Exception as e:
-            # This except block will catch Implicit exceptions:  
-            # Those raised through normal execution.
-            print(f"Data: {data.strip()} caused Exception: {e}")
-        finally:
+# Main batch processing function
+def main():
+    try:
+        # Open the file and process each line
+        with open("data/pixell_river_mortgages.txt", "r") as input_file:
             print("**************************************************")
+            
+            for line in input_file:
+                # Split the line into components
+                amount_str, rate_str, amortization_str, frequency_str = line.strip().split(",")
+                
+                try:
+                    # Convert string values to their respective types
+                    amount = float(amount_str)
+                    rate = MortgageRate[rate_str]
+                    frequency = MortgageFrequency[frequency_str]
+                    amortization = int(amortization_str)
+                    
+                    # Create a Mortgage instance
+                    mortgage = Mortgage(amount, rate, frequency, amortization)
+                    
+                    # Print the mortgage details
+                    print(mortgage)
+                
+                except ValueError as e:
+                    # Catch explicit exceptions raised in the Mortgage class
+                    print(f"Data: {line.strip()} caused Exception: {e}")
+                
+                except Exception as e:
+                    # Catch any other exceptions
+                    print(f"Data: {line.strip()} caused Exception: {e}")
+                
+                finally:
+                    print("**************************************************")
+    
+    except FileNotFoundError as e:
+        # Handle file not found error
+        print(f"Error opening the mortgages file: {e}")
+
+# Execute the batch processing function
+if __name__ == "__main__":
+    main()
